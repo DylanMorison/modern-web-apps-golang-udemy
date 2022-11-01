399f8fbd000e1fda455d51b39afcfad9592dbd65
Dylan Morison <dylan.morison@altruist.com>
2022-11-01 14:09:02 -0700

    base template

--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -5,10 +5,12 @@ import (
 	"net/http"
 )
 
+// Home is the handler for the home page
 func Home(w http.ResponseWriter, r *http.Request) {
-	render.RenderTemplate(w, "home.page.html")
+	render.RenderTemplate(w, "home.page.tmpl")
 }
 
+// About is the handler for the about page
 func About(w http.ResponseWriter, r *http.Request) {
-	render.RenderTemplate(w, "about.page.html")
+	render.RenderTemplate(w, "about.page.tmpl")
 }
--- pkg/render/render.go
+++ pkg/render/render.go
@@ -6,12 +6,11 @@ import (
 	"net/http"
 )
 
-// RenderTemplate renders templates using "html/template" library
+// RenderTemplate renders a template
 func RenderTemplate(w http.ResponseWriter, tmpl string) {
-	parsedTemplate, _ := template.ParseFiles("./templates/" + tmpl)
+	parsedTemplate, _ := template.ParseFiles("./templates/"+tmpl, "./templates/base.layout.tmpl")
 	err := parsedTemplate.Execute(w, nil)
 	if err != nil {
 		fmt.Println("error parsing template:", err)
-		return
 	}
 }
